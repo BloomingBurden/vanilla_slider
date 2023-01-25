6924eb0e55a1b00ff51723438c47b833c5e8936d
Blooming Burden <flowersoverskulls96@gmail.com>
2023-01-25 21:37:42 +0300

    Global change: added buttons, scrumbread, adaptive

--- slider.js
+++ slider.js
@@ -2,37 +2,133 @@ export function slider() {
     const slider = document.querySelector('.slider');
     const ul = slider.getElementsByTagName('ul')[0];
     const li = ul.getElementsByTagName('li');
+    const toggles = document.querySelector('.slider__toggles');
+    const sliderButtons = document.querySelector('.slider__buttons');
+
     let width = slider.parentElement.offsetWidth - parseFloat(window.getComputedStyle(slider.parentElement).paddingLeft) - parseFloat(window.getComputedStyle(slider.parentElement).paddingRight);
     let targetLeft = 0;
     let direction = 'left';
+
+    // Проверка, есть ли на сайте toggles
+    // Если есть, создать заданное количество
+    function hasToggles() {
+        if (!toggles) return;
+
+        const data = toggles.dataset.quantity ? toggles.dataset.quantity : 3;
+
+        for (let i = 0; i < data; i++) {
+            let li = document.createElement('li');
+
+            if (i === 0) {
+                li.dataset.active = 'true';
+            }
+            toggles.append(li);
+        }
+    }
+    hasToggles(); //Создать toggles
+
 
     // Функция, проверяющая на каких устройствах отключить слайдер
     // А на каких включить, в зависимости от стилей(slider--mob, slider--table)
     function allowedDevice() {
         width = slider.parentElement.offsetWidth - parseFloat(window.getComputedStyle(slider.parentElement).paddingLeft) - parseFloat(window.getComputedStyle(slider.parentElement).paddingRight);
-        const sliderMob  = slider.classList.contains('slider--mob');
-        const sliderTable =  slider.classList.contains('slider--table');
+        const sliderMob = slider.classList.contains('slider--mob');
+        const sliderTable = slider.classList.contains('slider--table');
 
         if (sliderMob || sliderTable) {
             if (sliderMob && window.innerWidth >= 768 || sliderTable && window.innerWidth >= 1200) {
                 slider.removeAttribute('style');
                 ul.removeAttribute('style');
-                
+                toggles.style.display = 'none';
+                sliderButtons.style.display = 'none';
+
                 for (let elem of li) {
                     elem.removeAttribute('style');
                 }
-
                 return;
             }
-        }
-
+        } 
         ul.ondragstart = () => false;
+        toggles.removeAttribute('style');
+        sliderButtons.removeAttribute('style');
         setBaseStylies();
-        ul.addEventListener('pointerdown', elemDown);
+        setToggle(0);
+        toggles ? toggles.addEventListener('click', selectElem) : false;
+        ul.addEventListener('pointerdown', elemDown); // Добавить ключевое событие
     }
     allowedDevice();
     window.addEventListener('resize', allowedDevice);
 
+    // Переключать по кнопке слайды
+    function btnSwapSlider() {
+        if (!sliderButtons) return;
+
+        sliderButtons.addEventListener('click', (event) => {
+            const target = event.target.closest('button');
+            let count = 0;
+
+            if (!target) return;
+
+            targetLeft = parseFloat(window.getComputedStyle(ul).left);
+
+            for (let elem of toggles.children) {
+                if (!elem.dataset.active) {
+                    count++;
+                } else {
+                    count - 1;
+                    break;
+                }
+            }
+
+            if (target.classList.contains('slider--prev')) {
+                direction = 'right';
+                ul.style.left = `${targetLeft + width}px`;
+                setToggle(count - 1 < 0 ? 0 : count - 1);
+                checkBorderLine();
+            }
+            if (target.classList.contains('slider--next')) {
+                direction = 'left';
+                ul.style.left = `${targetLeft - width}px`;
+                setToggle(count + 1);
+                checkBorderLine();
+            }
+        });
+    }
+    btnSwapSlider();
+
+    // Клик по переключателю. 
+    // Подсветка активного элемента
+    function selectElem(e) {
+        let target = e.target.closest('li');
+        let count = 0;
+
+        if (!target || !toggles) return;
+
+        for (let elem of toggles.children) {
+            elem.removeAttribute('data-active');
+        }
+        target.dataset.active = 'true';
+
+        for (let elem of toggles.children) {
+            if (elem.dataset.active) {
+                break;
+            }
+            count++;
+        }
+        ul.style.left = `${-width * count}px`;
+    }
+
+    //Смена toggles после свапа slider.
+    function setToggle(current) {
+        if (!toggles) return;
+
+        if (current > li.length - 1) return;
+
+        for (let elem of toggles.children) {
+            elem.removeAttribute('data-active');
+        }
+        toggles.children[current].dataset.active = 'true';
+    }
 
     // Задать базовые css свойства slider.
     // Задать базовые сss свойства Ul
@@ -57,6 +153,22 @@ export function slider() {
             elem.style.cssText = `
                 width: ${width}px;
             `
+        }
+    }
+
+
+    //Проверка на границы ul
+    // Не выходить за самый левый край ul
+    // Не выходить за самый правый край ul.
+    function checkBorderLine() {
+        if (direction === 'left') {
+            if (Math.abs(targetLeft) >= (li.length - 1) * width) {
+                ul.style.left = `${(li.length - 1) * -width}px`;
+            }
+        } else {
+            if (targetLeft >= 0) {
+                ul.style.left = `0`;
+            }
         }
     }
 
@@ -88,28 +200,14 @@ export function slider() {
         // pointerup задан на document, а не ul
         // Не происходит бага, когда палец улетает с ul и событие up теряется.
         function elemUp(event) {
-            let abs = direction === 'left' ? Math.abs(targetLeft / width) + 0.5 :  Math.abs(targetLeft / width) - 0.5;
+            let abs = direction === 'left' ? Math.abs(targetLeft / width) + 0.5 : Math.abs(targetLeft / width) - 0.5;
             document.removeEventListener('pointermove', elemMove);
             target.style.left = `${-Math.round(abs) * width}px`;
-             
+
             checkBorderLine();
+            setToggle(Math.round(abs));
             target.removeEventListener('pointerup', elemUp);
             document.removeEventListener('pointerup', elemUp);
-        }
-
-        //Проверка на границы ul
-        // Не выходить за самый левый край ul
-        // Не выходить за самый правый край ul.
-        function checkBorderLine() {
-            if (direction === 'left') {
-                if (Math.abs(targetLeft) >= (li.length - 1) * width) {
-                    target.style.left = `${(li.length - 1) * -width}px`;
-                }    
-            } else {
-                if (targetLeft >= 0) {
-                    target.style.left = `0`;
-                }
-            }    
         }
 
         document.addEventListener('pointerup', elemUp);
